a26b651fb214417833f86f5619b76c0472f66664
reginaldo-marinho <you@example.com>
2024-07-22 19:43:05 -0300

    Mantendo a resposabilidade de criar o grid exatamente para a classe Query

--- src/RuculaUp.WebUi/src/app/queryGrid/integranteGrid.ts
+++ src/RuculaUp.WebUi/src/app/queryGrid/integranteGrid.ts
@@ -1,40 +1,34 @@
 import { Rucula } from "@reginaldo-marinho/rucula-js";
-import { Query, QueryInit } from "./query";
+import { Query, QueryInit,gridConfig } from "./query";
 import { HttpGenericService } from "../http-generic/http-generic.service";
 import { TabulatorService } from "../tabulator/tabulator.service";
 
 export class IntegranteGrid extends Query implements QueryInit {
+    protected override setData(e: any): void {
+      throw new Error("Method not implemented.");
+    }
+   
+    constructor(rucula:Rucula, http:HttpGenericService, tab:TabulatorService) {
+        
 
-    constructor(rucula:Rucula, private http:HttpGenericService, private tab:TabulatorService) {
-        super(rucula, "IntegranteQueryPaged");
+      let gridConfig:gridConfig = {
+        URL:"http://localhost:5270/Query",
+        inputConfig: JSON.stringify({
+         LastId:"", 
+         LastNome:""
+       }),
+        columns: [
+          {title:"Id", field:"Id", width:140},
+          {title:"Nome", field:"Nome",  width: 200},
+          {title:"Ministerio", field:"Ministerio",  width:200},
+        ]
+      } 
+      super(http,rucula, tab, "IntegranteQueryPaged",gridConfig)
     }
   
     init(): void {
-      
-      this.inputOptions.options = JSON.stringify({
-        LastId:"", 
-        LastNome:""
-      })
-
-      this.queryView()
+      this.dataGrid()
     }
 
-    protected override queryView() {
-
-        this.http.post<any>("http://localhost:5270/Query", this.inputOptions)
-        .subscribe(result => {
-
-          this.inputOptions.options = result.options
-
-          this.tab.create(".r-act-grid-body", { 
-            columns:[
-              {title:"Id", field:"Id", width:140},
-              {title:"Nome", field:"Nome",  width: 200},
-              {title:"Ministerio", field:"Ministerio",  width:200},
-            ],
-            data: result.data
-          })
-          
-        });
-    }     
+  
 }
--- src/RuculaUp.WebUi/src/app/queryGrid/query.ts
+++ src/RuculaUp.WebUi/src/app/queryGrid/query.ts
@@ -1,4 +1,6 @@
 import { Rucula } from "@reginaldo-marinho/rucula-js";
+import { TabulatorService } from "../tabulator/tabulator.service";
+import { HttpGenericService } from "../http-generic/http-generic.service";
 
 export abstract class Query {
     
@@ -10,12 +12,18 @@ export abstract class Query {
         find:4
     }
 
-    private rucula:Rucula
-    protected inputOptions: inputOptions;
-    
-    constructor(rucula:Rucula, name:string, ) {
+    private inputOptions: inputOptions;
+    private gridConfig:gridConfig
+    protected rucula:Rucula
+    protected tab:TabulatorService
+    protected http:HttpGenericService
+
+    constructor(http:HttpGenericService, rucula:Rucula, tab:TabulatorService, name:string, gridConfig:gridConfig) {
         
+        this.http = http
         this.rucula = rucula
+        this.tab = tab
+        this.gridConfig = gridConfig
         
         this.inputOptions = {
             name: name,
@@ -34,24 +42,39 @@ export abstract class Query {
             
             let page = e.detail.page
             this.inputOptions.page = this.pageOption[page]
-            this.queryView()
+            this.dataGrid()
         })
         
         this.rucula.event.on('r-pagination-find',(e:CustomEvent)=> {
             this.inputOptions.text = e.detail.value
             this.inputOptions.page = this.pageOption.find
-            this.queryView()
+            this.dataGrid()
         })
     
           this.rucula.event.on('r-pagination-row',(e:CustomEvent)=> {
 
               let row = e.detail.row
               this.inputOptions.rowNumber = Number(row)
-              this.queryView()
+              this.dataGrid()
             })
-        }
+    }
         
-    protected abstract queryView():void;
+    protected dataGrid(){
+        
+        this.http.post<any>(this.gridConfig.URL, this.inputOptions)
+        .subscribe(result => {
+
+          this.inputOptions.options = result.options
+
+          var tabulator = this.tab.create(".r-act-grid-body", { 
+            columns:this.gridConfig.columns,
+            data: result.data
+          })
+
+        });
+    }
+
+    protected abstract setData(e:any):void;
 
 }
 
@@ -67,3 +90,12 @@ export type inputOptions = {
 export interface QueryInit{
     init():void;
 }
+
+
+export type gridConfig = {
+    URL: string
+    columns: any
+    inputConfig: any
+    
+}
+
